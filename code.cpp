10fc5e3a178833ea2986ff6c4c46ffa6044ac713
Yuvraj Jwala <74554809+yuvrajjwala@users.noreply.github.com>
2021-10-11 13:49:34 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void merge(int arr[],int l,int mid,int r){
-   int n1=mid-l;
+   int n1=mid-l+1; //You have to consider size here not the index.
    int n2=r-mid;
 
    int a[n1];
